a8c129fa2e958a9d4cdb5b2532dc3c2dc72d59fd
kunlexi <adesinataofik5@gmail.com>
2023-01-12 22:00:17 +0100

    first commit

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./Profile.module.css"
+
+const profile = () => {
+  return (
+    <div>
+        <h1>Profile</h1>
+    </div>
+  )
+}
+
+export default profile
